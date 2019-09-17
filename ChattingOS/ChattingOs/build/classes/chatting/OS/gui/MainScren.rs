8b34991ef0fa7150815010cff262d9f2a7e62e5f
Tamim Arefin Anik <42251521+tamim36@users.noreply.github.com>
2019-09-17 13:48:16 +0600

    initial commit

---
+++ ChattingOS/ChattingOs/build/classes/chatting/OS/gui/MainScren.rs
@@ -0,0 +1,7 @@
+chatting.os.gui.MainScren$2
+chatting.os.gui.MainScren$1
+chatting.os.gui.MainScren
+chatting.os.gui.MainScren$6
+chatting.os.gui.MainScren$5
+chatting.os.gui.MainScren$4
+chatting.os.gui.MainScren$3
---
+++ ChattingOS/ChattingOs/src/chatting/OS/Chatting.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatting.OS;
+public class Chatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        chattingOS.gui.ChattingOS screen = new chattingOS.gui.ChattingOS();
+       screen.show();
+    }
+    
+}
